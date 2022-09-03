4cf070a6a6ce425e100ee7d4f28af4c874e56e7d
kirillf2412 <kirillf2412@gmail.com>
2022-09-03 10:43:18 +0300

    Metro shop parser implemented

---
+++ src/ShopParsers/Metro/DetailsElementHelper.cs
@@ -0,0 +1,144 @@
+﻿using OpenQA.Selenium;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace ShopParsers.Metro
+{
+    internal static class DetailsElementHelper
+    {
+        public static string GetName(this IWebDriver webDriver)
+        {
+            return webDriver.FindElement(By.XPath("//h1[contains(@class,'title')]")).Text;
+        }
+        public static bool GetPrice(this IWebDriver webDriver, out decimal price, out decimal? discountPrice)
+        {
+            try
+            {
+                var priceElement = webDriver.FindElement(By.XPath("//aside[contains(@class,'product__aside')]"));
+                return GetPrice(priceElement, out price, out discountPrice);
+            }
+            catch
+            {
+                price = 0;
+                discountPrice = null;
+                return false;
+            }
+        }
+        public static bool GetPrice(this IWebElement priceElement, out decimal price, out decimal? discountPrice)
+        {
+            try
+            {
+                var prices = priceElement.FindElements(By.XPath("//span[contains(@class,'price__value')]"));
+                if (prices.Any())
+                {
+                    decimal.TryParse(Regex.Match(prices[0].Text, @"\d+([.,][0-9]{1,3})?").ValueSpan,
+                        NumberStyles.Any,
+                        CultureInfo.InvariantCulture,
+                        out price);
+                    if (prices.Count > 1 &&
+                        decimal.TryParse(
+                            Regex.Match(prices[1].Text, @"\d+([.,][0-9]{1,3})?").ValueSpan,
+                            NumberStyles.Any,
+                            CultureInfo.InvariantCulture,
+                            out var newDiscountPrice))
+                    {
+                        discountPrice = newDiscountPrice;
+                        return true;
+                    }
+                    discountPrice = null;
+                    return true;
+                }
+                discountPrice = null;
+                price = 0;
+                return false;
+            }
+            catch
+            {
+                discountPrice = null;
+                price = 0;
+                return false;
+            }
+        }
+        public static string? GetBrand(this IWebDriver webDriver)
+        {
+            try
+            {
+                return webDriver.FindElement(By.XPath("//li[div/span[contains(.,'Бренд')]]/span")).Text;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public static double? GetVolume(this IWebDriver webDriver)
+        {
+            try
+            {
+                var volumeString = webDriver.FindElement(By.XPath("//li[div/span[contains(.,'Объем')]]/span")).Text;
+                return double.Parse(Regex.Match(volumeString, @"\d+([.,][0-9]{1,3})?").ValueSpan);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public static bool? GetFiltration(this IWebDriver webDriver)
+        {
+            try
+            {
+                return webDriver.FindElement(By.XPath("//li[div/span[contains(.,'Объем')]]/span")).Text
+                    .Equals("фильтрованное", StringComparison.OrdinalIgnoreCase);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public static string? GetCountry(this IWebDriver webDriver)
+        {
+            try
+            {
+                return webDriver.FindElement(By.XPath("//li[div/span[contains(.,'Страна')]]/span")).Text;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public static string? GetColor(this IWebDriver webDriver)
+        {
+            try
+            {
+                return webDriver.FindElement(By.XPath("//li[div/span[contains(.,'Вид')]]/span")).Text;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public static bool? GetPasteurization(this IWebDriver webDriver)
+        {
+            try
+            {
+                return webDriver.FindElement(By.XPath("//li[div/span[contains(.,'Пастеризация')]]/span")).Text
+                .Equals("Пастеризованное", StringComparison.OrdinalIgnoreCase);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public static double? GetInitialWort(this IWebDriver webDriver)
+        {
+            try
+            {
+                var wortString = webDriver.FindElement(By.XPath("//li[div/span[contains(.,'Экстрактивность')]]/span")).Text;
+                return double.Parse(wortString);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ src/ShopParsers/Metro/MetroBeerParser.cs
@@ -0,0 +1,129 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ShopParsers.Metro
+{
+    public class MetroBeerParser : IDisposable
+    {
+        private readonly IWebDriver webDriver;
+        private Random random;
+        public MetroBeerParser(IWebDriver webDriver)
+        {
+            this.webDriver = webDriver;
+            random = new();
+            ConfigurePage();
+        }
+        public async Task<IEnumerable<ShopBeer>> ParseBeers()
+        {
+            var beerList = new List<ShopBeer>();
+            for (int i = 1; await GetBeersPerPage(i, beerList); i++) { }
+            return beerList;
+        }
+        public async Task<IEnumerable<ShopBeer>> ParseBeers(int startPage, int endPage)
+        {
+            var beerList = new List<ShopBeer>();
+            for (int i = startPage; i <= endPage; i++)
+            {
+                await GetBeersPerPage(i, beerList);
+            }
+            return beerList;
+        }
+        /// <summary>
+        /// Get beers in selected page
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="shopBeers"></param>
+        /// <returns>If can next true</returns>
+        private async Task<bool> GetBeersPerPage(int page, List<ShopBeer> shopBeers)
+        {
+            webDriver.Navigate().GoToUrl("https://online.metro-cc.ru/category/alkogolnaya-produkciya/pivo-sidr?order=popularity_desc&attributes=304%3Asteklo-304,zhb-304,alyuminievaya-banka-304" +
+                $"&page={page}");
+            var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(60));
+            wait.Until(drv => drv.FindElement(By.XPath("//main")));
+            var urls = GetDetailsUrls();
+            var canNext = webDriver.FindElements(By.XPath("//ul[contains(@class,'catalog-paginate')]/li[last()]/a/*[local-name() = 'svg']")).Any();
+            foreach (var url in urls)
+            {
+                try
+                {
+                    shopBeers.Add(ParseDetailsPage(url));
+                    await Task.Delay(random.Next(0, 3000));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Cant parse beer: " + ex.Message);
+                }
+            }
+            return canNext;
+
+        }
+        private IEnumerable<string> GetDetailsUrls()
+        {
+            var elements = webDriver.FindElements(By.XPath("//main//*[@class='base-product-item__content-details']/a"));
+            return elements.Select(c => c.GetAttribute("href")).ToList();
+        }
+        private ShopBeer ParseDetailsPage(string url)
+        {
+            webDriver.Navigate().GoToUrl(url);
+            var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(60));
+            wait.Until(drv => drv.FindElement(By.XPath("//h1[contains(@class,'title')]")));
+            var name = webDriver.GetName();
+            wait.Until(drv => drv.FindElement(By.XPath("//aside[contains(@class,'product__aside')]")));
+            var priceElement = webDriver.FindElement(By.XPath("//aside[contains(@class,'product__aside')]"));
+            var isAvalible = !priceElement.FindElements(By.XPath(".//div[contains(@class,'out-of-stocks')]")).Any();
+            decimal price = 0;
+            decimal? discountPrice = null;
+            if (isAvalible)
+            {
+                priceElement.GetPrice(out price, out discountPrice);
+            }
+            wait.Until(drv => drv.FindElement(By.XPath("//section[contains(@class,'additional-block')]")));
+            var brand = webDriver.GetBrand();
+            var volume = webDriver.GetVolume();
+            var filtration = webDriver.GetFiltration();
+            var country = webDriver.GetCountry();
+            var color = webDriver.GetColor();
+            var pasterization = webDriver.GetPasteurization();
+            double? initialWort = webDriver.GetInitialWort();
+
+            var beer = new ShopBeer(name, price)
+            {
+                Brand = brand,
+                Volume = volume,
+                Filtration = filtration,
+                Country = country,
+                Color = color,
+                DiscountPrice = discountPrice,
+                InitialWort = initialWort,
+                IsAvailable = isAvalible,
+                Pasteurization = pasterization
+            };
+            Console.WriteLine($"added: {beer.Name} {beer.Volume} {country}, f:{filtration}, pz:{pasterization} p:{price}, av:{isAvalible} br:{brand}");
+            return beer;
+        }
+        public void SetNewStore(int id)
+        {
+            webDriver.Manage().Cookies.AddCookie(new Cookie("pickupStore", id.ToString()));
+        }
+        private void ConfigurePage()
+        {
+            webDriver.Navigate().GoToUrl("https://online.metro-cc.ru");
+            var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(60));
+            wait.Until(drv => drv.FindElement(By.XPath("//*[@id=\"__layout\"]")));
+            var cookies = webDriver.Manage().Cookies;
+            var ageCookie = cookies.GetCookieNamed("18ageConfirm");
+            cookies.AddCookie(ChangeCookieValue(ageCookie, "true"));
+            cookies.AddCookie(new Cookie("pickupStore", "11"));
+            cookies.AddCookie(new Cookie("is18Confirmed", "true"));
+        }
+        private static Cookie ChangeCookieValue(Cookie oldCookie, string value)
+        {
+            return new Cookie(oldCookie.Name, value, oldCookie.Domain, oldCookie.Path,
+                oldCookie.Expiry, oldCookie.Secure, oldCookie.IsHttpOnly, oldCookie.SameSite);
+        }
+        public void Dispose()
+        {
+            webDriver.Dispose();
+        }
+    }
+}
