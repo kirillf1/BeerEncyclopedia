166916f11f7d28dc9a3a7cd672dfcd93fcdeb572
kirillf2412 <kirillf2412@gmail.com>
2022-09-03 11:45:51 +0300

    Shop added

---
+++ src/ShopParsers/Shop.cs
@@ -0,0 +1,19 @@
+﻿namespace ShopParsers
+{
+    public class Shop
+    {
+        public Shop(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+            ShopBeers = new();
+        }
+        public Shop(Guid id, string name,IEnumerable<ShopBeer> shopBeers) : this(id,name)
+        {
+            ShopBeers.AddRange(shopBeers);
+        }
+        public List<ShopBeer> ShopBeers { get; }
+        public Guid Id { get; }
+        public string Name { get; set; }
+    }
+}
