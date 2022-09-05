3d0a9593f088321bf1bcd7bc60188bb33de6ee8d
kirillf2412 <kirillf2412@gmail.com>
2022-09-05 21:42:59 +0300

    Implemented ShopBeerController

---
+++ src/ShopBeerService/Controllers/ShopBeerController.cs
@@ -0,0 +1,24 @@
+﻿using BeerShared.Data;
+using BeerShared.DTO;
+using Microsoft.AspNetCore.Mvc;
+using ShopBeerService.Queries;
+using ShopBeerService.Services;
+
+namespace ShopBeerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopBeerController : ControllerBase
+    {
+        private readonly BeerService beerService;
+        public ShopBeerController(BeerService beerService)
+        {
+            this.beerService = beerService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<ApiResult<ShopBeerInfo>>> GetBeers([FromQuery]ShopBeerQuery shopBeerQuery)
+        {
+            return await beerService.GetShopBeers(shopBeerQuery);
+        }
+    }
+}
