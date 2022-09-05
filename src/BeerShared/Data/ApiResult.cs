8122b965607091286ca298be68595a6b4c2928b5
kirillf2412 <kirillf2412@gmail.com>
2022-09-05 18:56:32 +0300

    ApiResult class was implemented

---
+++ src/BeerShared/Data/ApiResult.cs
@@ -0,0 +1,37 @@
+﻿namespace BeerShared.Data
+{
+    public class ApiResult<T>
+    {
+        public ApiResult(
+            List<T> data,
+            int count,
+            int pageIndex,
+            int pageSize)
+        {
+            Data = data;
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            TotalCount = count;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        }
+        public List<T> Data { get; private set; }
+        public int PageIndex { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalCount { get; private set; }
+        public int TotalPages { get; private set; }
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 0);
+            }
+        }
+        public bool HasNextPage
+        {
+            get
+            {
+                return ((PageIndex + 1) < TotalPages);
+            }
+        }
+    }
+}
