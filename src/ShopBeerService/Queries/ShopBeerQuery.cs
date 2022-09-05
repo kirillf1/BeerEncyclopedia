d0dd435d91135d54a0716e7feeb66f20ac4130c7
kirillf2412 <kirillf2412@gmail.com>
2022-09-05 21:40:52 +0300

    ShopBeerQuery added

---
+++ src/ShopBeerService/Queries/ShopBeerQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace ShopBeerService.Queries
+{
+    public class ShopBeerQuery
+    {
+        public int PageIndex { get; set; } = 0;
+        public int PageSize { get; set; } = 10;
+        public string? Name { get; set; }
+        public bool? IsAvalible { get; set; }
+        public bool? HasSourceBeer { get; set; }
+        public decimal? PriceMin { get; set; }
+        public decimal? PriceMax { get; set; }
+        public IEnumerable<Guid>? ShopIds { get; set; }
+    }
+}
