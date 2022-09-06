a3b606e0af333fae677009e2ebe1a6fb7abf30b0
kirillf2412 <kirillf2412@gmail.com>
2022-09-06 20:08:25 +0300

    StartTimeServiceArgs added

---
+++ src/BeerEncyclopedia.Tests/ShopBeerService/BeerShopParserTests/StartServiceArgsTests.cs
@@ -0,0 +1,25 @@
+﻿using ShopBeerService.Workers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerEncyclopedia.Tests.ShopBeerService.BeerShopParserTests
+{
+    public class StartTimeServiceArgsTests
+    {
+        [Fact]
+        public void GetScheduledDate_CurrentDayPlusHours_ReturnDifferenceHours()
+        {
+            var expectedHoursDifference = 2;
+            var timeZoneId = "Russian Standard Time";
+            var currentDate = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.Now, timeZoneId);
+            var serviceArgs = new StartTimeServiceArgs(currentDate.DayOfWeek,currentDate.Hour + expectedHoursDifference,timeZoneId);
+
+            var timeDifference = serviceArgs.GetDelayTime();
+            
+            Assert.Equal(expectedHoursDifference, Math.Round( timeDifference.TotalHours, MidpointRounding.ToPositiveInfinity));
+        }
+    }
+}
---
+++ src/ShopBeerService/Workers/StartTimeServiceArgs.cs
@@ -0,0 +1,36 @@
+﻿namespace ShopBeerService.Workers
+{
+    public class StartTimeServiceArgs
+    {
+        public StartTimeServiceArgs(DayOfWeek DayOfWeek, int hour, string timeZoneId = "Russian Standard Time")
+        {
+            this.DayOfWeek = DayOfWeek;
+            if (hour < 0 || hour > 24)
+                throw new ArgumentException("Invalid hour value");
+            Hour = hour;
+            TimeZoneId = timeZoneId;
+        }
+
+        public DayOfWeek DayOfWeek { get; }
+        public int Hour { get; }
+        public string TimeZoneId { get; set; }
+
+        public TimeSpan GetDelayTime()
+        {
+            return GetScheduledDate().Subtract(GetCurrentDate());
+        }
+        private DateTime GetScheduledDate()
+        {
+            var dateTime = GetCurrentDate();
+            var offset = DayOfWeek - dateTime.DayOfWeek;
+            if (offset < 0)
+                offset = 7 + offset;
+            return dateTime.AddDays(offset).
+                Date.Add(new TimeSpan(Hour, 0, 0));      
+        }
+        private DateTime GetCurrentDate()
+        {
+           return TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.Now, TimeZoneId);
+        }
+    }
+}
