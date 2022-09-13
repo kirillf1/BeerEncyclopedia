d6f932588f8472e86ecdc50ae78d305da8a6052d
kirillf2412 <kirillf2412@gmail.com>
2022-09-13 16:39:17 +0300

    Added constructor

--- src/BeerEncyclopedia.Infrastructure/Data/BeerEncyclopediaDbContext.cs
+++ src/BeerEncyclopedia.Infrastructure/Data/BeerEncyclopediaDbContext.cs
@@ -5,6 +5,9 @@ namespace BeerEncyclopedia.Infrastructure.Data
 {
     public abstract class BeerEncyclopediaDbContext : DbContext
     {
+        public BeerEncyclopediaDbContext(DbContextOptions options) : base(options)
+        {
+        }
         public abstract DbSet<Beer> Beers { get; }
         public abstract DbSet<Color> Colors { get; }
         public abstract DbSet<Country> Countries { get; }
