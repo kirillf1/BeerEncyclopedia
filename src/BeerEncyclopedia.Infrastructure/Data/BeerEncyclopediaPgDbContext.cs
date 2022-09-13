59f9a5372f5580604d4ccc0212bfe367841a655d
kirillf2412 <kirillf2412@gmail.com>
2022-09-13 16:39:55 +0300

    Added PG DbContext with migration

---
+++ src/BeerEncyclopedia.Infrastructure/Data/BeerEncyclopediaPgDbContext.cs
@@ -0,0 +1,30 @@
+﻿using BeerEncyclopedia.Domain;
+using BeerEncyclopedia.Infrastructure.Data.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeerEncyclopedia.Infrastructure.Data
+{
+    public class BeerEncyclopediaPgDbContext : BeerEncyclopediaDbContext
+    {
+        public BeerEncyclopediaPgDbContext(DbContextOptions<BeerEncyclopediaPgDbContext> options) : base(options)
+        {
+
+        }
+        public override DbSet<Beer> Beers => Set<Beer>();
+        public override DbSet<Color> Colors => Set<Color>();
+        public override DbSet<Country> Countries => Set<Country>();
+        public override DbSet<Style> Styles => Set<Style>();
+        public override DbSet<Manufacturer> Manufacturers => Set<Manufacturer>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasPostgresExtension("uuid-ossp");
+
+            modelBuilder.ApplyConfiguration(new BeerConfiguration());
+            modelBuilder.ApplyConfiguration(new ColorConfiguration());
+            modelBuilder.ApplyConfiguration(new StyleConfiguration());
+            modelBuilder.ApplyConfiguration(new ManufacturerConfiguration());
+            modelBuilder.ApplyConfiguration(new CountryConfiguration());
+        }
+    }
+}
---
+++ src/BeerEncyclopedia.Infrastructure/Data/Migrations/20220913123425_init.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeerEncyclopedia.Infrastructure.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:uuid-ossp", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "Colors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(400)", maxLength: 400, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Beers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    AltName = table.Column<string>(type: "text", nullable: true),
+                    CountryId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Rating = table.Column<double>(type: "double precision", nullable: false),
+                    CreationTime = table.Column<DateOnly>(type: "date", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Beers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Beers_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BeerImages",
+                columns: table => new
+                {
+                    BeerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MainImageUrl = table.Column<string>(type: "text", nullable: false),
+                    ImageUrls = table.Column<List<string>>(type: "text[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BeerImages", x => x.BeerId);
+                    table.ForeignKey(
+                        name: "FK_BeerImages_Beers_BeerId",
+                        column: x => x.BeerId,
+                        principalTable: "Beers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChemicalIndicators",
+                columns: table => new
+                {
+                    BeerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Filtration = table.Column<bool>(type: "boolean", nullable: false),
+                    Pasteurization = table.Column<bool>(type: "boolean", nullable: false),
+                    Strength = table.Column<double>(type: "double precision", nullable: false),
+                    InitialWort = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChemicalIndicators", x => x.BeerId);
+                    table.ForeignKey(
+                        name: "FK_ChemicalIndicators_Beers_BeerId",
+                        column: x => x.BeerId,
+                        principalTable: "Beers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Manufacturers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(600)", maxLength: 600, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    CountryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PictureUrl = table.Column<string>(type: "text", nullable: true),
+                    BeerId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Manufacturers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Manufacturers_Beers_BeerId",
+                        column: x => x.BeerId,
+                        principalTable: "Beers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Manufacturers_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrganolepticIndicators",
+                columns: table => new
+                {
+                    BeerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ColorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Taste = table.Column<string>(type: "text", nullable: false),
+                    Bitterness = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrganolepticIndicators", x => x.BeerId);
+                    table.ForeignKey(
+                        name: "FK_OrganolepticIndicators_Beers_BeerId",
+                        column: x => x.BeerId,
+                        principalTable: "Beers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrganolepticIndicators_Colors_ColorId",
+                        column: x => x.ColorId,
+                        principalTable: "Colors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Styles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    NameEn = table.Column<string>(type: "character varying(400)", maxLength: 400, nullable: false),
+                    NameRus = table.Column<string>(type: "character varying(400)", maxLength: 400, nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    BeerId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Styles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Styles_Beers_BeerId",
+                        column: x => x.BeerId,
+                        principalTable: "Beers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Beers_AltName",
+                table: "Beers",
+                column: "AltName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Beers_CountryId",
+                table: "Beers",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Beers_Id",
+                table: "Beers",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Beers_Name",
+                table: "Beers",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Manufacturers_BeerId",
+                table: "Manufacturers",
+                column: "BeerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Manufacturers_CountryId",
+                table: "Manufacturers",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Manufacturers_Id",
+                table: "Manufacturers",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Manufacturers_Name",
+                table: "Manufacturers",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrganolepticIndicators_ColorId",
+                table: "OrganolepticIndicators",
+                column: "ColorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Styles_BeerId",
+                table: "Styles",
+                column: "BeerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BeerImages");
+
+            migrationBuilder.DropTable(
+                name: "ChemicalIndicators");
+
+            migrationBuilder.DropTable(
+                name: "Manufacturers");
+
+            migrationBuilder.DropTable(
+                name: "OrganolepticIndicators");
+
+            migrationBuilder.DropTable(
+                name: "Styles");
+
+            migrationBuilder.DropTable(
+                name: "Colors");
+
+            migrationBuilder.DropTable(
+                name: "Beers");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
