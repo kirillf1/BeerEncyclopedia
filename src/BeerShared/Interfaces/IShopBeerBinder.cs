76136d36e9e0879a678712a132b728f3c103f408
kirillf2412 <kirillf2412@gmail.com>
2022-09-20 17:49:18 +0300

    Added IShopBeerBinder

---
+++ src/BeerShared/Interfaces/IShopBeerBinder.cs
@@ -0,0 +1,11 @@
+﻿using BeerShared.DTO;
+
+namespace BeerShared.Interfaces
+{
+    public interface IShopBeerBinder
+    {
+        Task BindBeers(BindedSourceBeer bind, CancellationToken cancellationToken);
+        Task<IEnumerable<ShopBeerInfo>> GetNotBindedShopBeers(SourceBeerDetailsToBind sourceBeerDetails, CancellationToken cancellationToken);
+        Task<bool> TryBindShopBeers(SourceBeerDetailsToBind sourceBeerDetails, CancellationToken cancellationToken);
+    }
+}
--- src/ShopBeerService/Services/SourceBeerServices/SourceBeerBinder.cs
+++ src/ShopBeerService/Services/SourceBeerServices/SourceBeerBinder.cs
@@ -1,14 +1,17 @@
 ﻿using BeerFormaters.BeerNameFormater;
 using BeerShared.DTO;
+using BeerShared.Interfaces;
 using Cyrillic.Convert;
 using Microsoft.EntityFrameworkCore;
+using ShopBeerService.Converters;
 using ShopBeerService.Infrastructure;
+using ShopParsers;
 
 namespace ShopBeerService.Services.SourceBeerServices
 {
-    public class SourceBeerBinder
+    public class ShopBeerBinder : IShopBeerBinder
     {
-        public SourceBeerBinder(ShopBeerPGDbContext context)
+        public ShopBeerBinder(ShopBeerPGDbContext context)
         {
             this.context = context;
         }
@@ -25,22 +28,33 @@ namespace ShopBeerService.Services.SourceBeerServices
             }
             await context.SaveChangesAsync(cancellationToken);
         }
+        public async Task<IEnumerable<ShopBeerInfo>> GetNotBindedShopBeers(SourceBeerDetailsToBind sourceBeerDetails,
+            CancellationToken cancellationToken)
+        {
+            var shopBeers = await GetNotBindedShopBeers(sourceBeerDetails.Name, cancellationToken);
+            return shopBeers.Select(b => BeerInfoConverter.ConvertToShopBeerInfo(b));
+        }
         public async Task<bool> TryBindShopBeers(SourceBeerDetailsToBind sourceBeerDetails, CancellationToken cancellationToken)
         {
-            var conversion = new Conversion();
-            var nameFormater = new BeerNameFormater();
-            var beerNameFormated = nameFormater.Format(sourceBeerDetails.Name).Name;
-            var beerNameEn = conversion.RussianCyrillicToLatin(beerNameFormated);
-            var shopBeers = await context.ShopBeers.
-                Where(c => c.SourceBeerId == null
-                      && EF.Functions.ToTsVector("english", c.FormatedName).Matches(beerNameEn)).
-                ToArrayAsync(cancellationToken);
+            var shopBeers = await GetNotBindedShopBeers(sourceBeerDetails.Name, cancellationToken);
             foreach (var shopBeer in shopBeers)
             {
                 shopBeer.SourceBeerId = sourceBeerDetails.SourceBeerId;
             }
             await context.SaveChangesAsync(cancellationToken);
             return shopBeers.Length > 0;
+        }
+        private async Task<ShopBeer[]> GetNotBindedShopBeers(string sourceBeerName, CancellationToken cancellationToken)
+        {
+            var conversion = new Conversion();
+            var nameFormater = new BeerNameFormater();
+            var beerNameFormated = nameFormater.Format(sourceBeerName).Name;
+            var beerNameEn = conversion.RussianCyrillicToLatin(beerNameFormated);
+            return await context.ShopBeers.
+               Where(c => c.SourceBeerId == null
+                     && EF.Functions.ToTsVector("english", c.FormatedName).Matches(beerNameEn)).
+               ToArrayAsync(cancellationToken);
+
         }
     }
 }
