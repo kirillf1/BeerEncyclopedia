cba571118366f57fad0aa3948b265a0d5b107403
kirillf2412 <kirillf2412@gmail.com>
2022-09-20 15:48:43 +0300

    Added json deserialize support

--- src/BeerShared/Data/ApiResult.cs
+++ src/BeerShared/Data/ApiResult.cs
@@ -1,24 +1,32 @@
-﻿namespace BeerShared.Data
+﻿using System.Text.Json.Serialization;
+
+namespace BeerShared.Data
 {
     public class ApiResult<T>
     {
+        [JsonConstructor]
         public ApiResult(
             List<T> data,
-            int count,
+            int totalCount,
             int pageIndex,
             int pageSize)
         {
             Data = data;
             PageIndex = pageIndex;
             PageSize = pageSize;
-            TotalCount = count;
-            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            TotalCount = totalCount;
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
         }
-        public List<T> Data { get; private set; }
-        public int PageIndex { get; private set; }
-        public int PageSize { get; private set; }
-        public int TotalCount { get; private set; }
-        public int TotalPages { get; private set; }
+        [JsonInclude]
+        public List<T> Data { get; init; }
+        [JsonInclude]
+        public int PageIndex { get; init; }
+        [JsonInclude]
+        public int PageSize { get; init; }
+        [JsonInclude]
+        public int TotalCount { get; init; }
+        [JsonInclude]
+        public int TotalPages { get; init; }
         public bool HasPreviousPage
         {
             get
