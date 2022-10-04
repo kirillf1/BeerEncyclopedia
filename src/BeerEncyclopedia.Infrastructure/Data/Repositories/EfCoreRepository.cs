aec31607666427c73b49e3f42ff46f67d7109f2a
kirillf2412 <kirillf2412@gmail.com>
2022-10-04 20:26:10 +0300

    Ef core repository was added

---
+++ src/BeerEncyclopedia.Infrastructure/Data/Repositories/EfCoreRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification;
+using Ardalis.Specification.EntityFrameworkCore;
+using BeerEncyclopedia.Domain;
+
+namespace BeerEncyclopedia.Infrastructure.Data.Repositories
+{
+    public class EfCoreRepository<T> : RepositoryBase<T>, IReadRepositoryBase<T>, IRepository<T> where T : Entity
+    {
+        public EfCoreRepository(BeerEncyclopediaDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
