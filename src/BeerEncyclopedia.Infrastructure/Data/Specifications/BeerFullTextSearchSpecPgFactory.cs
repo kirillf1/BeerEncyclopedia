6647da610d58abb03747806db30854eae55f4ad9
kirillf2412 <kirillf2412@gmail.com>
2022-10-04 20:24:22 +0300

    Init name search specific postgres specificationFactory

---
+++ src/BeerEncyclopedia.Infrastructure/Data/Specifications/BeerFullTextSearchSpecPgFactory.cs
@@ -0,0 +1,17 @@
+﻿using Ardalis.Specification;
+using BeerEncyclopedia.Application.Specifications;
+using BeerEncyclopedia.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeerEncyclopedia.Infrastructure.Data.Specifications
+{
+    public class BeerFullTextSearchSpecPgFactory : INameSearchSpecificationFactory<Beer>
+    {
+        public Specification<Beer> GetByNameSpecification(string name)
+        {
+            return new EntityByNameSpec<Beer>(name, (t, c) =>
+            c.Where(p => EF.Functions.ToTsVector("russian", p.Name + " " + p.AltName)
+               .Matches(t)));
+        }
+    }
+}
---
+++ src/BeerEncyclopedia.Infrastructure/Data/Specifications/ManufacturerLikeSearchPgFactory.cs
@@ -0,0 +1,16 @@
+﻿using Ardalis.Specification;
+using BeerEncyclopedia.Application.Specifications;
+using BeerEncyclopedia.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeerEncyclopedia.Infrastructure.Data.Specifications
+{
+    public class ManufacturerLikeSearchPgFactory : INameSearchSpecificationFactory<Manufacturer>
+    {
+        public Specification<Manufacturer> GetByNameSpecification(string name)
+        {
+            return new EntityByNameSpec<Manufacturer>(name, (t, c) =>
+           c.Where(p => EF.Functions.ILike(p.Name,$"%{t}%")));
+        }
+    }
+}
