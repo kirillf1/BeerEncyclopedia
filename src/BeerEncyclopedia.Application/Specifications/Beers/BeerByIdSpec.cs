159a91ea35e4b2bc4f5e6d0cf448cdd45cb55ce1
kirillf2412 <kirillf2412@gmail.com>
2022-10-18 18:08:40 +0300

    Added specification beer by id

---
+++ src/BeerEncyclopedia.Application/Specifications/Beers/BeerByIdSpec.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.Specification;
+using BeerEncyclopedia.Domain;
+
+namespace BeerEncyclopedia.Application.Specifications.Beers
+{
+    public class BeerByIdSpec : Specification<Beer>
+    {
+        public BeerByIdSpec(Guid id)
+        {
+            Query.Where(c => c.Id == id)
+                .Include(c => c.Manufacturers)
+                .Include(c => c.ChemicalIndicators)
+                .Include(c => c.OrganolepticIndicators)
+                .Include(c => c.Country)
+                .Include(c => c.Styles)
+                .Include(c => c.BeerImages);
+        }
+    }
+}
