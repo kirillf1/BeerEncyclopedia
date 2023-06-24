0ab03643e3dffb67fd884e57fd504fd8162654cd
kirillf2412 <kirillf2412@gmail.com>
2023-06-24 15:05:16 +0300

    t

--- src/BeerEncyclopedia.API/Program.cs
+++ src/BeerEncyclopedia.API/Program.cs
@@ -1,3 +1,21 @@
+using BeerEncyclopedia.Application.BeersServices.EditorServices;
+using BeerEncyclopedia.Application.BeersServices.SearchServices;
+using BeerEncyclopedia.Application.BeersServices.SearchServices.ByIdSearchServices;
+using BeerEncyclopedia.Application.BeersServices.SearchServices.ByNamesSearchServices;
+using BeerEncyclopedia.Application.BeersServices.SearchServices.QuerySearchServices;
+using BeerEncyclopedia.Application.ColorServices;
+using BeerEncyclopedia.Application.CountryServices;
+using BeerEncyclopedia.Application.ManufacturerServices;
+using BeerEncyclopedia.Application.ShopBeerServices;
+using BeerEncyclopedia.Application.Specifications;
+using BeerEncyclopedia.Application.StyleServices;
+using BeerEncyclopedia.Domain;
+using BeerEncyclopedia.Infrastructure.Data;
+using BeerEncyclopedia.Infrastructure.Data.Repositories;
+using BeerEncyclopedia.Infrastructure.Data.Specifications;
+using BeerShared.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -5,7 +23,39 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BeerEncyclopediaPgDbContext>(c => c.UseNpgsql(builder.Configuration.GetConnectionString("DbConnectionString")));
+builder.Services.AddHttpClient("ShopBeer", c => c.BaseAddress = new Uri("https://localhost:5001/api/ShopBeer/"));
+builder.Services.AddHttpClient("ShopBeerBind", c => c.BaseAddress = new Uri("https://localhost:5001/api/ShopBeerBind/"));
+builder.Services.AddScoped<BeerEncyclopediaDbContext, BeerEncyclopediaPgDbContext>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(EfCoreRepository<>));
+builder.Services.AddScoped<INameSearchSpecificationFactory<Beer>, BeerFullTextSearchSpecPgFactory>();
+builder.Services.AddScoped<INameSearchSpecificationFactory<Manufacturer>, ManufacturerLikeSearchPgFactory>();
 
+builder.Services.AddScoped<IBeerEditorService, BeerEditorRepositoryService>();
+
+builder.Services.AddScoped<IBeerSearchByIdService,BeerSearchByIdRepositoryService>();
+builder.Services.AddScoped<IBeerSearchByQueryService, BeerSearchByQueryRepositoryService>();
+builder.Services.AddScoped<IBeerSearchByNamesService, BeerSearchByNamesRepositoryService>();
+
+builder.Services.AddScoped<IManufacturerSearchService,ManufacturerSearchService>();
+builder.Services.AddScoped<IStyleSearchService, StyleSearchService>();
+
+builder.Services.AddScoped<IColorSearchService, ColorSearchRepositoryService>();
+
+builder.Services.AddScoped<ICountrySearchService, CountrySearchRepositoryService>();
+
+builder.Services.AddTransient<IShopBeerService, ShopBeerServiceHttp>(s =>
+{
+    var client = s.GetRequiredService<IHttpClientFactory>().CreateClient("ShopBeer");
+    return new ShopBeerServiceHttp(client);
+}
+) ;
+builder.Services.AddTransient<IShopBeerBinder, ShopBeerBinderHttp>(s =>
+{
+    var client = s.GetRequiredService<IHttpClientFactory>().CreateClient("ShopBeerBind");
+    return new ShopBeerBinderHttp(client);
+}
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- src/BeerEncyclopedia.UI/MauiProgram.cs
+++ src/BeerEncyclopedia.UI/MauiProgram.cs
@@ -1,4 +1,15 @@
-﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+﻿using BeerEncyclopedia.Application.BeersServices.EditorServices;
+using BeerEncyclopedia.Application.BeersServices.SearchServices;
+using BeerEncyclopedia.Application.BeersServices.SearchServices.ByIdSearchServices;
+using BeerEncyclopedia.Application.BeersServices.SearchServices.ByImageSearchServices;
+using BeerEncyclopedia.Application.BeersServices.SearchServices.ByNamesSearchServices;
+using BeerEncyclopedia.Application.BeersServices.SearchServices.QuerySearchServices;
+using BeerEncyclopedia.Application.ColorServices;
+using BeerEncyclopedia.Application.CountryServices;
+using BeerEncyclopedia.Application.ImageSearchServices;
+using BeerEncyclopedia.Application.ManufacturerServices;
+using BeerEncyclopedia.Application.StyleServices;
+using MudBlazor.Services;
 
 namespace BeerEncyclopedia.UI
 {
@@ -16,8 +27,48 @@ namespace BeerEncyclopedia.UI
 
             builder.Services.AddMauiBlazorWebView();
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
+            builder.Services.AddMudServices();
+            builder.Services.AddHttpClient("Beers", c => c.BaseAddress = new Uri("https://localhost:5001/api/Beers/query/"));
+            builder.Services.AddHttpClient<IBeerSearchByNamesService, BeerSearchByNamesHttpService>(c =>
+            c.BaseAddress = new("https://localhost:5001/api/Beers/names"));
+            builder.Services.AddHttpClient<IBeerSearchByIdService, BeerSearchByIdHttpService>(c =>
+        c.BaseAddress = new("https://localhost:5001/api/Beers/"));
+            builder.Services.AddHttpClient<IBeerEditorService, BeerEditorHttpService>(c =>
+        c.BaseAddress = new("https://localhost:5001/api/Beers/"));
+
+            builder.Services.AddHttpClient<ICountrySearchService, CountrySearchHttpService>(c =>
+           c.BaseAddress = new("https://localhost:5001/api/Countries"));
+            builder.Services.AddHttpClient<IColorSearchService, ColorSearchHttpService>(c =>
+         c.BaseAddress = new("https://localhost:5001/api/Colors"));
+            builder.Services.AddScoped<IBeerSearchByImageService, BeerSearchByImageService>();
+            builder.Services.AddScoped<IBeerSearchByQueryService>(s =>
+            {
+                var client = s.GetRequiredService<IHttpClientFactory>().CreateClient("Beers");
+                return new BeerSearchByQueryHttpService(client);
+            });
+            builder.Services.AddHttpClient("Manufacturers", c => c.BaseAddress = new Uri("https://localhost:5001/api/Manufacturers/"));
+            
+            builder.Services.AddTransient<IManufacturerSearchService, ManufacuterSearchHttpService>(s =>
+            {
+                var client = s.GetRequiredService<IHttpClientFactory>().CreateClient("Manufacturers");
+                return new ManufacuterSearchHttpService(client);
+            });
+            
+            builder.Services.AddHttpClient<IImageSearchSevice, YandexNameByImageSearchService>(c =>
+            {
+                c.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36");
+                c.DefaultRequestHeaders.Add("sec-ch-ua-platform", "\"Windows\"");
+                c.DefaultRequestHeaders.Add("sec-ch-ua", "\"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"104\"");
+                c.DefaultRequestHeaders.Add("accept-language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7");
+            });
+            builder.Services.AddHttpClient("Styles", c => c.BaseAddress = new Uri("https://localhost:5001/api/Styles/"));
+            builder.Services.AddTransient<IStyleSearchService, StyleSearchHttpService>(s =>
+            {
+                var client = s.GetRequiredService<IHttpClientFactory>().CreateClient("Styles");
+                return new StyleSearchHttpService(client);
+            });
             return builder.Build();
         }
     }
